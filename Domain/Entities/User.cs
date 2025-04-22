774a9356079fcae2422ed4f35a92a76099f5893e
Le Huy <huylh0603@gmail.com>
2025-04-22 08:21:39 +0700

    user and role

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string? EmailToken { get; set; }
+        public DateTime? VerifyAt { get; set; }
+
+        public int RoleId { get; set; }
+        public Role Role { get; set; }  // 1 User có 1 Role
+    }
+}
